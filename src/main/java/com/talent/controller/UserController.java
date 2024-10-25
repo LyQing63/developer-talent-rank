c700dbef9404ce26aefa996a321dd951297443a9
LyQing63 <1827304875@qq.com>
2024-10-25 17:49:40 +0800

    添加了获取单个用户的信息

--- src/main/java/com/talent/controller/UserController.java
+++ src/main/java/com/talent/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.talent.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.talent.common.BaseResponse;
 import com.talent.common.ErrorCode;
 import com.talent.common.ResultUtils;
@@ -9,11 +10,11 @@ import com.talent.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author 郑皓天
@@ -46,5 +47,39 @@ public class UserController {
         return ResultUtils.success(user1);
     }
 
+    @GetMapping("/get_developer")
+    public BaseResponse get_developer(String login) {
+
+        // 从Redis中获取
+        Object userCache = redisManager.getUserInfo(login);
+        if (userCache == null) {
+            // 从数据库中搜索
+            QueryWrapper queryWrapper = new QueryWrapper<>();
+            queryWrapper.eq("login", login);
+            User userSaved = userService.getOne(queryWrapper);
+            if (userSaved == null) {
+                // 数据库中也没有数据
+                User userFromGithub = userService.getUserFromGithub(login);
+                // 不存在这个用户
+                if (userFromGithub == null) {
+                    return ResultUtils.error(ErrorCode.PARAMS_ERROR, "用户不存在");
+                }
+
+                // 数据库中没有数据，保存
+                boolean save = userService.save(userFromGithub);
+                if (!save) {
+                    return ResultUtils.error(ErrorCode.SYSTEM_ERROR, "数据库存储失败");
+                }
+                // 再缓存到redis中
+                redisManager.cacheUserInfo(login, userFromGithub, 30, TimeUnit.MINUTES);
+
+                return ResultUtils.success(userFromGithub);
+
+            }
+            return ResultUtils.success(userSaved);
+        }
+
+        return ResultUtils.success(userCache);
+    }
 
 }
