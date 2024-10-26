e59aec276011168daa52499c11e7225d1ea20d35
LyQing63 <1827304875@qq.com>
2024-10-26 11:57:18 +0800

    前后端互联，生成openapi

---
+++ web/src/services/ant-design-pro/typings.d.ts
@@ -0,0 +1,25 @@
+declare namespace API {
+  type BaseResponse = {
+    code?: number;
+    data?: Record<string, any>;
+    message?: string;
+  };
+
+  type getDeveloperParams = {
+    login: string;
+  };
+
+  type GrantedAuthority = {
+    authority?: string;
+  };
+
+  type loginParams = {
+    user: OAuth2User;
+  };
+
+  type OAuth2User = {
+    authorities?: GrantedAuthority[];
+    attributes?: Record<string, any>;
+    name?: string;
+  };
+}
