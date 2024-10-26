7eea184d66b78ba25414beb2560ccf13ad1042ac
LyQing63 <1827304875@qq.com>
2024-10-26 19:23:43 +0800

    登录逻辑修改

--- src/main/java/com/talent/job/DeveloperCrawler.java
+++ src/main/java/com/talent/job/DeveloperCrawler.java
@@ -1,11 +1,27 @@
 package com.talent.job;
 
+import com.talent.model.bo.DeveloperSearchResult;
+import com.talent.utils.GitHubDeveloperRankUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import java.util.PriorityQueue;
 
 @Slf4j
 @Service
 public class DeveloperCrawler {
 
+    private static final String ADMIN_TOKEN = "###########################";
+
+    @Scheduled(cron = "0 0 2 * * MON")
+    private static void scheduleWeeklyTask() {
+        GitHubDeveloperRankUtils.fetchTop100Repositories(ADMIN_TOKEN);
+    }
+
+    public static void main(String[] args) {
+        PriorityQueue<DeveloperSearchResult> developerSearchResults = GitHubDeveloperRankUtils.fetchTop100Repositories(ADMIN_TOKEN);
+        System.out.println(developerSearchResults);
+    }
 
 }
