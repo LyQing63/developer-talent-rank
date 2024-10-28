a05d4d12f80b8af1518c1593d7e91c05a5242870
LyQing63 <1827304875@qq.com>
2024-10-28 18:43:40 +0800

    添加redis缓存RatingResultVO的方法

--- src/main/java/com/talent/manager/RedisManager.java
+++ src/main/java/com/talent/manager/RedisManager.java
@@ -1,8 +1,8 @@
 package com.talent.manager;
 
 import com.talent.model.dto.User;
+import com.talent.model.vo.RatingResultVO;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
@@ -14,24 +14,36 @@ import java.util.concurrent.TimeUnit;
 public class RedisManager {
     private static final String USER_CACHE_KEY_PREFIX = "user:";
 
+    private static final long USER_CACHE_TIME_DAYS = 30;
+
     private static final String DEVELOPER_CACHE_KEY_PREFIX = "developer:";
 
+    private static final long DEVELOPER_CACHE_TIME_MINUTES = 30;
+
+    private static final String RATING_CACHE_KEY_PREFIX = "rating:";
+
+    private static final long RATING_CACHE_TIME_MINUTES = 30;
+
     @Resource
-    private RedisTemplate<String, User> redisTemplate;
+    private RedisTemplate<String, Object> redisTemplate;
 
     // 设置用户信息缓存
-    public void cacheUserInfo(String account, User userInfo, long timeout, TimeUnit unit) {
-        cacheInfo(USER_CACHE_KEY_PREFIX, account, userInfo, timeout, unit);
+    public void cacheUserInfo(String account, User userInfo) {
+        cacheInfo(USER_CACHE_KEY_PREFIX, account, userInfo, USER_CACHE_TIME_DAYS, TimeUnit.DAYS);
     }
 
-    public void cacheDeveloperInfo(String account, User userInfo, long timeout, TimeUnit unit) {
-        cacheInfo(DEVELOPER_CACHE_KEY_PREFIX, account, userInfo, timeout, unit);
+    public void cacheDeveloperInfo(String account, User userInfo) {
+        cacheInfo(DEVELOPER_CACHE_KEY_PREFIX, account, userInfo, DEVELOPER_CACHE_TIME_MINUTES, TimeUnit.MINUTES);
     }
 
-    // 设置开发者信息缓存
-    public void cacheInfo(String prefix, String account, User userInfo, long timeout, TimeUnit unit) {
+    public void cacheDeveloperRankInfo(String account, RatingResultVO ratingResultVO) {
+        cacheInfo(RATING_CACHE_KEY_PREFIX, account, ratingResultVO, RATING_CACHE_TIME_MINUTES, TimeUnit.MINUTES);
+    }
+
+    // 设置信息缓存
+    public void cacheInfo(String prefix, String account, Object info, long timeout, TimeUnit unit) {
         String key = prefix + account;
-        redisTemplate.opsForValue().set(key, userInfo, timeout, unit);
+        redisTemplate.opsForValue().set(key, info, timeout, unit);
     }
 
     public Object getInfo(String prefix, String account) {
@@ -43,9 +55,14 @@ public class RedisManager {
     public Object getUserInfo(String account) {
         return getInfo(USER_CACHE_KEY_PREFIX, account);
     }
-    // 获取用户信息缓存
+    // 获取开发者信息缓存
     public Object getDeveloperInfo(String account) {
         return getInfo(DEVELOPER_CACHE_KEY_PREFIX, account);
+    }
+
+    // 获取排名信息缓存
+    public Object getDeveloperRankingInfo(String account) {
+        return getInfo(RATING_CACHE_KEY_PREFIX, account);
     }
 
     public void removeInfo(String prefix, String account) {
@@ -61,4 +78,9 @@ public class RedisManager {
     public void removeDeveloperInfo(String account) {
         removeInfo(DEVELOPER_CACHE_KEY_PREFIX, account);
     }
+
+    public void removeDeveloperRankingInfo(String account) {
+        removeInfo(RATING_CACHE_KEY_PREFIX, account);
+    }
+
 }
