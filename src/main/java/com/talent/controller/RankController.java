aa4b85d98a37e0b3261bfa7f6953c879d41fd86b
LyQing63 <1827304875@qq.com>
2024-10-28 18:44:41 +0800

    给获取分数信息的接口添加缓存

--- src/main/java/com/talent/controller/RankController.java
+++ src/main/java/com/talent/controller/RankController.java
@@ -2,18 +2,20 @@ package com.talent.controller;
 
 import cn.hutool.json.JSONObject;
 import com.talent.common.BaseResponse;
-import com.talent.common.ErrorCode;
 import com.talent.common.ResultUtils;
+import com.talent.manager.RedisManager;
 import com.talent.model.dto.User;
 import com.talent.model.vo.RatingResultVO;
 import com.talent.model.vo.RatingVO;
 import com.talent.utils.GitHubDeveloperRankUtils;
+import com.talent.utils.TokenUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 @RestController
@@ -21,16 +23,18 @@ import java.util.List;
 @Slf4j
 public class RankController {
 
+    @Resource
+    private RedisManager redisManager;
+
     @GetMapping("/score")
     public BaseResponse getRatingResult(String account, @RequestHeader(value = "Authorization") String authorization) {
 
-        String[] s = authorization.split(" ");
-
-        if (s.length < 2) {
-            return ResultUtils.error(ErrorCode.PARAMS_ERROR, "未登录");
+        String token = TokenUtils.getToken(authorization);
+        // 从redis缓存中读取
+        RatingResultVO developerRankingInfo = (RatingResultVO) redisManager.getDeveloperRankingInfo(account);
+        if (developerRankingInfo != null) {
+            return ResultUtils.success(developerRankingInfo);
         }
-
-        String token = s[1];
 
         JSONObject developerInfo = GitHubDeveloperRankUtils.getDeveloperInfo(account, token);
         User developer = User.parseUser(developerInfo);
@@ -39,6 +43,9 @@ public class RankController {
         RatingResultVO ratingResultVO = new RatingResultVO();
         ratingResultVO.setRatingResults(ratingResults);
         ratingResultVO.setTotalScore(totalScore);
+
+        // 存入缓存中
+        redisManager.cacheDeveloperRankInfo(account, ratingResultVO);
 
         return ResultUtils.success(ratingResultVO);
     }
